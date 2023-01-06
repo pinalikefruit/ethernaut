79a086f34226bb0e63de646b2944133f72bcc51f
José Piña <32434364+pinalikefruit@users.noreply.github.com>
2023-01-06 15:15:21 +0100

    [ADD] waitForTransaction

--- test/staging/telephone.staging.test.ts
+++ test/staging/telephone.staging.test.ts
@@ -1,13 +1,11 @@
 import { assert } from "chai"
-import {network, ethers, getNamedAccounts } from "hardhat"
+import { network, ethers, getNamedAccounts } from "hardhat"
 import { developmentChains, networkConfig} from "../../helper-hardhat-config"
-import {  Attack,Telephone } from "../../typechain-types"
+import {  Attack ,Telephone } from "../../typechain-types"
 import "dotenv/config"
 import abi from "../../artifacts/contracts/Telephone.sol/Telephone.json"
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const PRIVATE_KEY = String(process.env.PRIVATE_KEY)
-
 
 developmentChains.includes(network.name)
     ? describe.skip
@@ -20,7 +18,7 @@ developmentChains.includes(network.name)
 
         beforeEach(async() => {
             provider =  new ethers.providers.JsonRpcProvider(GOERLI_RPC_URL);
-            hacker  = new ethers.Wallet(PRIVATE_KEY,provider)
+            hacker = (await getNamedAccounts()).hacker
             
             addressContract = networkConfig[network.config.chainId!]["contractAddress"]!
             
@@ -31,10 +29,7 @@ developmentChains.includes(network.name)
         describe("Hack", function() {
             it("Claimed ownership of the contract", async() => {
                 const tx = await attack.hack()
-                tx.wait(1)
-                setTimeout(()=>{
-                    console.log("Waiting......")
-                }, 15000)
+                await provider.waitForTransaction(tx.hash)
                 // Check new owner 
                 let newOwner = await telephone.owner()
                 assert.equal(newOwner, hacker.address)
